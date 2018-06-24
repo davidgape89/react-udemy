a76b07558c43eeab99acbff8ec4a23837b522f79
davidgape89 <davidgape89@gmail.com>
2018-06-24 22:44:59 +0100

    Lesson 56

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -11,7 +11,6 @@ export default class AddOption extends React.Component {
 
     handleAddOption(event) {
         event.preventDefault();
-
         const option = event.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
         
