5cdc7e122a12c1a3fc6d96ae51d38eb43dbff6e7
davidgape89 <davidgape89@gmail.com>
2018-07-28 23:21:45 +0100

    Lesson 68

--- src/components/Action.js
+++ src/components/Action.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Action = (props) => (
     <div>
         <button 
+            className="big-button"
             disabled={props.isOptionsEmpty} 
             onClick={props.handlePick}>
             What do
--- src/components/Option.js
+++ src/components/Option.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Option = (props) => (
     <div>
         {props.option} 
-        <button onClick={() => props.handleDeleteOption(props.option)}>
+        <button 
+            className="button button--link"
+            onClick={() => props.handleDeleteOption(props.option)}>
             Delete
         </button>
     </div>
