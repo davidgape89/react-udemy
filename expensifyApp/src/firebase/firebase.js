051b7a48ed314ff382688ae519945e3418e67a4f
davidgape89 <davidgape89@gmail.com>
2018-09-19 12:08:36 +0100

    Lesson 156

--- expensifyApp/src/firebase/firebase.js
+++ expensifyApp/src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyB1rLHf4AE9HG9277nwp_stLibWDz_2K0M",
-    authDomain: "expensify-7c4d5.firebaseapp.com",
-    databaseURL: "https://expensify-7c4d5.firebaseio.com",
-    projectId: "expensify-7c4d5",
-    storageBucket: "expensify-7c4d5.appspot.com",
-    messagingSenderId: "435998829748"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
@@ -14,99 +14,3 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 export {firebase, database as default};
-
-// // const onChange = (snapshot) => {
-// //     const expenses = [];
-
-// //     snapshot.forEach((childSnapshot) => {
-// //         expenses.push({
-// //             id: childSnapshot.key,
-// //             ...childSnapshot.val()
-// //         });
-// //     });
-    
-// //     console.log(expenses);
-// // };
-
-// // const onSomething = (snapshot) => {
-// //     console.log(snapshot.val());
-// // };
-
-// // const expenses = database.ref('expenses');
-
-// // //database.ref('expenses').on('value', onChange);
-
-// // //expenses.on('value', onSomething);
-
-// // expenses.on('child_removed', onSomething);
-
-// // expenses.on('child_changed', onSomething);
-
-// // expenses.on('child_added', onSomething);
-
-// // // database.ref('expenses').push({
-// // //     description: 'Description 1',
-// // //     note: '',
-// // //     amount: 1234
-// // // });
-
-
-
-
-// // // database.ref('notes').push({
-// // //     title: 'Course topics',
-// // //     body: 'Several topics'
-// // // });
-
-// // // const firebaseNotes = {
-// // //     notes: {
-
-// // //     }
-// // // }
-
-// // // const callback = database.ref().on('value', (snapshot) => {
-// // //     const value = snapshot.val();
-// // //     console.log(`${value.name} is a ${value.job.title} at ${value.job.company}`);
-// // // });
-
-// // // database.ref().on('value', (snapshot) => {
-// // //     const value = snapshot.val();
-// // //     console.log(`is a ${value.job.title} at ${value.job.company}`);
-// // // });
-
-// // // database.ref().off('value', callback);
-
-// // // database.ref().once('value')
-// // //     .then((snapshot) => {
-// // //         console.log(snapshot.val());
-// // //     })
-// // //     .catch((error) => {
-// // //         console.log('Error fetching data', error);
-// // //     });
-
-// // // database.ref().set({
-// // //     name: 'Davie boi',
-// // //     age: 29,
-// // //     stressLevel: 5,
-// // //     isSingle: false,
-// // //     job: {
-// // //         title: 'Software engineer',
-// // //         company: 'Google'
-// // //     },
-// // //     location: {
-// // //         city: 'Krakow',
-// // //         country: 'Poland'
-// // //     }
-// // // }).then((data) => {
-// // //     console.log('Data saved');
-// // //     console.log(data);
-// // // }).catch((data) => {
-// // //     console.log('Error');
-// // //     console.log(data);
-// // // });
-
-// // // database.ref().update({
-// // //     stressLevel: 9,
-// // //     'job/company': 'Amazon',
-// // //     'location/city': 'London'
-// // // });
