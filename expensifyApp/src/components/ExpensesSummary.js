e5a44fedea95cce09224e0f114d263b4f7deab0b
davidgape89 <davidgape89@gmail.com>
2018-09-25 20:26:57 +0200

    Lesson 170

--- expensifyApp/src/components/ExpensesSummary.js
+++ expensifyApp/src/components/ExpensesSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import numeral from 'numeral';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import expensesTotalSelector from '../selectors/expenses-total';
 import expensesSelector from '../selectors/expenses';
 
@@ -8,8 +9,15 @@ export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
     const expenseWord = expenseCount === 1? 'expense': 'expenses';
 
     return (
-        <div>
-            <h1>Viewing {expenseCount} {expenseWord} totalling {numeral(expensesTotal / 100).format('$0,0.00')}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">
+                    Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{numeral(expensesTotal / 100).format('$0,0.00')}</span>
+                </h1>
+                <div className="page-header__actions">
+                    <Link to="/create" className="button">Add Expense</Link>
+                </div>
+            </div>
         </div>
     );
 }
